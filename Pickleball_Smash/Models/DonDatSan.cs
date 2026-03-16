74c20bead2e23c720090694ff9d2162068d206a9
duy280503 <duynguyen030528@gmail.com>
2026-03-16 22:06:55 +0700

    rename project Smart to Smash

---
+++ Pickleball_Smash/Models/DonDatSan.cs
@@ -0,0 +1,21 @@
+﻿namespace Pickleball_Smash.Models
+{
+    public class DonDatSan
+    {
+        public int DonDatSanID { get; set; }
+        public int? NguoiDungID { get; set; }
+        public int? SanID { get; set; }
+        public DateTime NgayChoi { get; set; }
+        public string KhungGio { get; set; } = null!;
+        public decimal? TongTien { get; set; }
+        public string? TrangThaiDon { get; set; }
+        public DateTime NgayTao { get; set; }
+
+        // Navigation properties
+        public NguoiDung? NguoiDung { get; set; }
+        public SanPickleball? SanPickleball { get; set; }
+        public ICollection<ChiTietDichVu>? ChiTietDichVus { get; set; }
+        public ICollection<ThanhToan>? ThanhToans { get; set; }
+    }
+}
+
---
+++ Pickleball_Smash/Models/LichSuChat.cs
@@ -0,0 +1,15 @@
+﻿namespace Pickleball_Smash.Models
+{
+    public class LichSuChat
+    {
+        public int ChatID { get; set; }
+        public int? NguoiDungID { get; set; }
+        public string? NoiDungHoi { get; set; }
+        public string? PhanHoiAI { get; set; }
+        public DateTime ThoiGian { get; set; }
+
+        // Navigation properties
+        public NguoiDung? NguoiDung { get; set; }
+    }
+}
+
